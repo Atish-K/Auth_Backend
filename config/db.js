0e5b077fd847575cebd7b38d34a68a4481fb2141
Atish Kumar <atish.kumar@grssl.net>
2025-03-13 14:14:15 +0530

    update

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://atish0518:NJSjbyYuYmp5oXX0@adminapp.cn4is.mongodb.net/adminApp");
+        await mongoose.connect("mongodb+srv://atish0518:NJSjbyYuYmp5oXX0@adminapp.cn4is.mongodb.net/adminapp?retryWrites=true&w=majority");
         console.log('MongoDB connected successfully');
     } catch (error) {
         console.error('Error connecting to MongoDB:', error.message);
